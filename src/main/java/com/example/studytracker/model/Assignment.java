132e24ff4f5652d2b74e6991659d3f66f198bdd5
Kunal Gangani <kunalganganu009@gmail.com>
2026-08-11 16:44:52 +0530

    Added Models

---
+++ src/main/java/com/example/studytracker/model/Assignment.java
@@ -0,0 +1,7 @@
+package com.example.studytracker.model;
+
+public enum Assignment {
+    NOT_STARTED,
+    IN_PROGRESS,
+    COMPLETED
+}
---
+++ src/main/java/com/example/studytracker/model/Priority.java
@@ -0,0 +1,7 @@
+package com.example.studytracker.model;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
