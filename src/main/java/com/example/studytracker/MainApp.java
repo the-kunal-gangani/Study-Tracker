71bcc47e77e7a48ba80a7dce650890a47ec841cd
Kunal Gangani <kunalganganu009@gmail.com>
2026-08-15 01:41:01 +0530

    Solved the Blank Values issue in Name FIeld

--- src/main/java/com/example/studytracker/MainApp.java
+++ src/main/java/com/example/studytracker/MainApp.java
@@ -6,7 +6,9 @@ import com.example.studytracker.model.Subject;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -18,11 +20,32 @@ public class MainApp extends Application {
         dbManager.initializeTables();
 
         SubjectDao subjectDao = new SubjectDao(dbManager);
+
         ListView<String> listView = new ListView<>();
         for (Subject s : subjectDao.getAllSubjects()) {
             listView.getItems().add(s.getName());
         }
-        VBox root = new VBox(listView);
+
+        TextField nameField = new TextField();
+        nameField.setPromptText("Subject Name");
+
+        Button addButton = new Button("Add Subject");
+        addButton.setOnAction(event -> {
+            String name = nameField.getText();
+            if (name.isBlank()) {
+                return;
+            }
+            Subject subject = new Subject(0, name, "");
+            subjectDao.addSubject(subject);
+
+            listView.getItems().clear();
+            for (Subject s : subjectDao.getAllSubjects()) {
+                listView.getItems().add(s.getName());
+            }
+            nameField.clear();
+        });
+
+        VBox root = new VBox(nameField, addButton, listView);
         Scene scene = new Scene(root, 600, 400);
 
         primaryStage.setTitle("MCA Study Tracker");
