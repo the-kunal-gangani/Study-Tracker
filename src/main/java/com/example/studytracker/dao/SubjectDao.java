071408ffa73690dc4d83c5bc10452f7d3cfde5fa
Kunal Gangani <kunalganganu009@gmail.com>
2026-08-14 13:49:02 +0530

    Added ResultSets and PreparedStatements in Subject DAO

--- src/main/java/com/example/studytracker/dao/SubjectDao.java
+++ src/main/java/com/example/studytracker/dao/SubjectDao.java
@@ -2,6 +2,8 @@ package com.example.studytracker.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.example.studytracker.db.DatabaseManager;
 import com.example.studytracker.model.Subject;
@@ -16,10 +18,37 @@ public class SubjectDao {
 
     public void addSubject(Subject subject) {
         String sql = "INSERT INTO subjects (name, instructor) VALUES (?, ?)";
-        try {
-            Connection conn = dbManager.connect();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-        } catch (Exception e) {
+        try (Connection conn = dbManager.connect();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, subject.getName());
+            stmt.setString(2, subject.getInstructor());
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("...", e);
         }
+    }
+
+    public java.util.List<Subject> getAllSubjects() {
+        String sql = "SELECT * FROM subjects";
+        java.util.List<Subject> subjects = new java.util.ArrayList<>();
+
+        try (Connection conn = dbManager.connect();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String instructor = rs.getString("instructor");
+
+                Subject subject = new Subject(id, name, instructor);
+                subjects.add(subject);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to fetch subjects", e);
+        }
+
+        return subjects;
     }
 }
